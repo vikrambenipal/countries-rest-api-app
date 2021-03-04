36fb07b27f3662ada993196ff005b2704b98c662
vikrambenipal <vikrambenipal@gmail.com>
2021-03-04 10:21:27 -0500

    basic layout for card component complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Dropdown from './components/Dropdown';
 import Search from './components/Search';
 import Card from './components/Card';
 import axios from 'axios';
+import { convertTypeAcquisitionFromJson } from 'typescript';
 
 function App() {
 
@@ -32,7 +33,10 @@ function App() {
       <Search handleSearch={handleSearch}/>
       <Dropdown handleRegion={handleRegion}/>
       {countries.map((country, i) => {
-        return ((region == "" || region == country.region) && country.name.includes(search)) && <Card key={i} name={country.name} />
+        return ((region == "" || region == country.region) && country.name.includes(search)) 
+        && <Card key={i} name={country.name} population={country.population} region={country.region} capital={country.capital} 
+        nativeName={country.nativeName} subregion={country.subregion} languages={country.languages} currencies={country.currencies} 
+        topLevelDomain={country.topLevelDomain} borders={country.borders} flag={country.flag}/>
       })}
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import styled from 'styled-components';
 
-const Card = ({ name }) => {
+const CardContainer = styled.div`
+    border: 1px solid black;
+    margin: 0 auto;
+    margin-bottom: 25px;
+    width: 70%;
+    img {
+        width: 100%;
+    }
+`
+
+const Card = ({ name, population, region, capital, nativeName, subregion, 
+    languages, currencies, topLevelDomain, borders, flag }) => {
 
     return (
-        <div>
-            <p>{name}</p>
-        </div>
+        <CardContainer>
+            <img src={flag} alt=""></img>
+            <h3>{name}</h3>
+            <p>Population: {population}</p>
+            <p>Region: {region}</p>
+            <p>Capital: {capital}</p>
+        </CardContainer>
     )
 }
 export default Card;
