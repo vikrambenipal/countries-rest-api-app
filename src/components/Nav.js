f402e87eac8b5fe57a56f54ceff187d408a462a9
vikrambenipal <vikrambenipal@gmail.com>
2021-03-05 14:27:09 -0500

    font awesome moon icon added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon } from '@fortawesome/free-solid-svg-icons'
 
 const NavContainer = styled.div`
     border: 1px solid black;
@@ -14,12 +16,26 @@ const NavContainer = styled.div`
         margin-right: 20px;
     }
 `
+const DarkMode = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    cursor: pointer;
+    .moon {
+        margin-right: 15px;
+        stroke: black;
+        stroke-width: 25;
+    }
+`
 
 const Nav = () => {
     return (
         <NavContainer>
             <h3>Where in the world?</h3>
-            <p>Dark Mode</p>
+            <DarkMode>
+                <FontAwesomeIcon className="moon" icon={faMoon} color="white" stroke="black"/>
+                <p>Dark Mode</p>
+            </DarkMode>
         </NavContainer>
     )
 }
